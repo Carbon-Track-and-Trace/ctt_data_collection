4c679d90a380b67ccf8702b71991b3b01c7ef8e2
Patrick Merlot <patrik.merlot@gmail.com>
2016-11-17 13:48:58 +0100

    update cronjobs

--- watchdogs/daily_backup.sh
+++ watchdogs/daily_backup.sh
@@ -12,12 +12,12 @@ DATE=`date +%Y-%m-%d`;
 PERIOD='daily'
 
 ## Backup a local database structure without data using ‘msqldump’
-/opt/numascale/bin/msqldump -p 54321 --database=ctt  --describe | gzip > /home/numascale/BACKUP_MONETDB/$PERIOD/bkp_Structure_TK_$DATE.sql.gz
-/opt/numascale/bin/msqldump -p 50000 --database=ctt  --describe | gzip > /home/numascale/BACKUP_MONETDB/$PERIOD/bkp_Structure_VJ_$DATE.sql.gz
+msqldump -p 54321 --database=ctt  --describe | gzip > /home/numascale/BACKUP_MONETDB/$PERIOD/bkp_Structure_TK_$DATE.sql.gz
+msqldump -p 50000 --database=ctt  --describe | gzip > /home/numascale/BACKUP_MONETDB/$PERIOD/bkp_Structure_VJ_$DATE.sql.gz
 
 ## Backup a local database with data
-/opt/numascale/bin/mclient -p 54321 -d ctt --dump | gzip > /home/numascale/BACKUP_MONETDB/$PERIOD/bkp_DUMP_TK_$DATE.sql.gz
-/opt/numascale/bin/mclient -p 50000 -d ctt --dump | gzip > /home/numascale/BACKUP_MONETDB/$PERIOD/bkp_DUMP_VJ_$DATE.sql.gz
+mclient -p 54321 -d ctt --dump | gzip > /home/numascale/BACKUP_MONETDB/$PERIOD/bkp_DUMP_TK_$DATE.sql.gz
+mclient -p 50000 -d ctt --dump | gzip > /home/numascale/BACKUP_MONETDB/$PERIOD/bkp_DUMP_VJ_$DATE.sql.gz
 
 
 echo `date` - Backup complete
